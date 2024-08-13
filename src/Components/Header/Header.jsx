b3c4e906421051b1029772d4d7d7390be44416c1
Jay Kalani <jaykalani13@gmail.com>
2024-08-13 17:19:08 +0530

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-content">
+        <h2>Order your favourite food here</h2>
+        <p>Satisfy your cravings with our diverse menu, crafted with the freshest ingredients and a passion for flavor. From savory to sweet, we've got it all! Enjoy fast, reliable delivery and exceptional service every time you order. Taste the difference today!</p>
+        <a href='#explore-menu'>View Menu</a>
+      </div>
+    </div>
+  )
+}
+
+export default Header
